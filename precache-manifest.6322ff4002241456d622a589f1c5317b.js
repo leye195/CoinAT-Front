57c8ac023d97d2f218687571106c0c3bb832c9e8
leye195 <leye195@naver.com>
2020-04-14 19:27:44 +0900

    Updates

---
+++ precache-manifest.6322ff4002241456d622a589f1c5317b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9525d20a163bbf46d70a7c53e352effb",
+    "url": "/CoinAT-Front/index.html"
+  },
+  {
+    "revision": "d626c408edef248b23a1",
+    "url": "/CoinAT-Front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2d8a959bf6814725ef4f",
+    "url": "/CoinAT-Front/static/js/2.7ccc950d.chunk.js"
+  },
+  {
+    "revision": "6fa9a282f4d19bdc4ece7110cc83f5b7",
+    "url": "/CoinAT-Front/static/js/2.7ccc950d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d626c408edef248b23a1",
+    "url": "/CoinAT-Front/static/js/main.b269de16.chunk.js"
+  },
+  {
+    "revision": "10fccc6c76ef923d47f0",
+    "url": "/CoinAT-Front/static/js/runtime-main.9ffde30a.js"
+  }
+]);
