2149db99f83646fe24ea339d87ed25ca77e36ce7
leye195 <leye195@naver.com>
2020-07-17 13:40:15 +0900

    Updates

--- precache-manifest.c59fdbea4cb0984f0d25dbf0f56f7ba7.js
+++ precache-manifest.c59fdbea4cb0984f0d25dbf0f56f7ba7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e024db66be43a6253bc1fc7fb8bf36d",
+    "revision": "40533eb7be9cd276ca7b258eab0a01a2",
     "url": "/CoinAT-Front/index.html"
   },
   {
-    "revision": "2176dcc29b9ae7fd6909",
+    "revision": "8fc7dfd5ec9b72f0b75d",
     "url": "/CoinAT-Front/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CoinAT-Front/static/js/2.6fc77a12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2176dcc29b9ae7fd6909",
-    "url": "/CoinAT-Front/static/js/main.bc831351.chunk.js"
+    "revision": "8fc7dfd5ec9b72f0b75d",
+    "url": "/CoinAT-Front/static/js/main.d25b5f10.chunk.js"
   },
   {
     "revision": "10fccc6c76ef923d47f0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CoinAT-Front/precache-manifest.16fde9e4f6feafb28641efe8f186de93.js"
+  "/CoinAT-Front/precache-manifest.c59fdbea4cb0984f0d25dbf0f56f7ba7.js"
 );
 
 self.addEventListener('message', (event) => {
