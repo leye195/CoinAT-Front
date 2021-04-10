9dc41dc70b839eeb635301b6d624e9969c5be04f
leye195 <leye195@naver.com>
2021-04-10 20:02:48 +0900

    feat: add EventHeader

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation, NavLink, useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
 import qs from 'qs'
 import EventContainer from '../components/Event/EventContainer';
@@ -20,6 +20,29 @@ const Container = styled.div`
   padding-top: 2.5rem;
   padding-bottom: 2.5rem;
   min-height: 744px;
+`;
+
+const EventHeader = styled.div`
+  height: 50px;
+  background-color: #ffffff;
+  border-bottom: 1.5px solid rgb(222 222 222 / 50%);
+
+  & > div {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    height: 50px;
+    width: 90vw;
+
+    & a {
+      color: black;
+      text-decoration: none;
+    }
+
+    & .event-notice {
+      font-weight: bold;
+    }
+  }
 `;
 
 const NoticeSection= styled.section`
@@ -56,9 +79,11 @@ const Event = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const location = useLocation();
     const history = useHistory();
+    const match = useRouteMatch();
+
     const dispatch = useDispatch();
     const {notices = [], isLoading = true, more} = useSelector((state)=>state.notice);
-    
+    const {params} = match;
 
     const handleLoadMore = () =>{
       dispatch(loadNotice({page: currentPage+1,type}));
@@ -66,9 +91,11 @@ const Event = () => {
     }
     
     useEffect(() => {
+ 
       const type = qs.parse(location.search)['?type'];
+
       if(!type){
-        history.replace('/event?type=notice');
+        history.replace(`${location.pathname}?type=notice`);
         return;
       }
       setType(type);
@@ -77,8 +104,14 @@ const Event = () => {
     useEffect(()=>{
       dispatch(loadNotice({page:1,type: type? type:'notice'}));
     },[dispatch,type]);
-
-    return <Container>
+    return (
+    <>
+      <EventHeader>
+        <div>
+          <NavLink activeClassName={params.name==='upbit'&&"event-notice"} to={`/event/upbit`}>업비트</NavLink>
+        </div>
+      </EventHeader>
+      <Container>
         <EventContainer>
             <EventSideBar/>
             <NoticeSection>
@@ -86,7 +119,7 @@ const Event = () => {
                   <NoticeHeader>{convertTitle[type]}</NoticeHeader>
                   {notices?.length>0&&(
                     <>
-                      <NoticeTable items={notices}/>
+                      <NoticeTable items={notices} type={type}/>
                       <LoadMoreContainer>
                         <LoadMore isMore={more} handleLoadMore={handleLoadMore}/>
                       </LoadMoreContainer>
@@ -97,6 +130,8 @@ const Event = () => {
             </NoticeSection>
         </EventContainer>
         {isLoading&&<Loading isLoading={true}/>}
-    </Container>
+      </Container>
+    </>
+    )
 };
 export default Event;
