0eac98770dbcd294feb90a60be5c120877b58a5a
leye195 <leye195@naver.com>
2021-05-30 18:43:18 +0900

    Updates

---
+++ precache-manifest.feffd2f06b6e2bda6194f0c8e74476d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb66e671026041577109ced61c1f79e",
+    "url": "/CoinAT-Front/index.html"
+  },
+  {
+    "revision": "cfcb74e06a63a8323c6a",
+    "url": "/CoinAT-Front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78bd10385f75d89c5a9c",
+    "url": "/CoinAT-Front/static/js/2.09e9d46f.chunk.js"
+  },
+  {
+    "revision": "df0c3399103d503b397e3acd8ee7cd60",
+    "url": "/CoinAT-Front/static/js/2.09e9d46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcb74e06a63a8323c6a",
+    "url": "/CoinAT-Front/static/js/main.ca36826d.chunk.js"
+  },
+  {
+    "revision": "10fccc6c76ef923d47f0",
+    "url": "/CoinAT-Front/static/js/runtime-main.9ffde30a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7466ae356ebde3d9d6f570a20b635229.js"
+  "/CoinAT-Front/precache-manifest.feffd2f06b6e2bda6194f0c8e74476d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CoinAT-Front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
